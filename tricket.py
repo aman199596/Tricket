aab39e2ad8cc009be2326b637c35414dc16d21af
Aman <aman199596@gmail.com>
2018-01-12 16:35:55 +0530

    Added menus to tricket.py

--- tricket.py
+++ tricket.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan  9 03:44:16 2018
+Created on Fri Jan 12 15:25:26 2018
 
 @author: aman
 """
@@ -11,30 +11,61 @@ import time
 import urllib
 from bs4 import BeautifulSoup
 
-# Print waiting message
-print('Retrieving scores...')
 
-
-while True:
-    # cricinfo website
-    cricbuzz = 'http://www.cricbuzz.com/'
-
-    # query the website and return the html to variable 'page'
-    page = urllib.request.urlopen(cricbuzz)
+def show_score(page, n):
+    print('Retrieving score..')
     
-    # parse the html using beautiful soup and store in variable 'soup'
-    soup = BeautifulSoup(page, 'html.parser')
-    
-    # Retrieve the scores from html
-    name_box = soup.find_all('div', attrs={'class' : 'cb-col cb-col-25 cb-mtch-blk'})
-    
-    # Clear the terminal
-    os.system('clear')
-    
-    for i in name_box:
-        name = i.text.strip()
-        print(name)
-        print()
+    while True:
+        soup = get_soup(page)
+        match_box = soup.find_all('div', attrs={'class' : 'cb-col cb-col-25 cb-mtch-blk'})
         
-    # Wait 50s before refreshing
-    time.sleep(50)
+        os.system('clear')
+        
+        if n == 0 :
+            for match in match_box:
+                score = match.text.strip()
+                print(score)
+                print()
+        else:
+            score = match_box[n - 1].text.strip()
+            print(score)
+            
+        time.sleep(50)
+
+
+def print_match_list(soup) :
+    count = 0
+    print(str(count) + ' : ' + 'Show all matches')
+    
+    match_box = soup.find_all('div', attrs={'class' : 'cb-col cb-col-25 cb-mtch-blk'})
+    
+    for match in match_box:
+        count += 1
+        print(str(count) + ' : ' + match.a.get('title'))
+        
+    
+
+def get_soup(page):
+    # query the website and return the html to variable 'page'
+    page = urllib.request.urlopen(page)
+    
+    # parse the html using beautiful soup and return it
+    return BeautifulSoup(page, 'html.parser')
+
+
+def run_tricket(page):
+    print('Retrieving matches...')
+    
+    soup = get_soup(page)
+    
+    print_match_list(soup)
+    
+    print()
+    
+    option = input('Enter your choice: ')
+    
+    show_score(page, int(option))
+        
+
+if __name__ == "__main__":
+    run_tricket('http://www.cricbuzz.com/')
